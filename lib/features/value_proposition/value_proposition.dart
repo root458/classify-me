6f231f05c8744c4e7f2777ca255c8acb9d50f14b
Hope Kagame <hopekagame1@gmail.com>
2023-04-30 00:00:20 +0300

    Add page views

--- lib/features/value_proposition/value_proposition.dart
+++ lib/features/value_proposition/value_proposition.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
 
+class ValueProposition extends StatelessWidget {
+  const ValueProposition({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
