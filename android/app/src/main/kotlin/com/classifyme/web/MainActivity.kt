25ef87e50df456c2cb2b3b76ad804f218f21a421
Hope Kagame <hopekagame1@gmail.com>
2023-04-29 23:44:47 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/classifyme/web/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.classifyme.web.classifyme
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:classifyme/app/app.dart';
+import 'package:classifyme/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
