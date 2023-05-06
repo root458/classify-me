7107281cb919414e38eb61855b6ecddb6cbe9b4a
Hope Kagame <hopekagame1@gmail.com>
2023-05-07 00:22:45 +0300

    * Update gitignore * Add error types

---
+++ lib/models/errors.dart
@@ -0,0 +1,47 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'errors.freezed.dart';
+
+@freezed
+class ClassifyMeValidationError with _$ClassifyMeValidationError {
+  factory ClassifyMeValidationError(
+    int code,
+    List<ClassifyMeFieldValidationError> errors,
+  ) = _ClassifyMeValidationError;
+
+  factory ClassifyMeValidationError.fromJson(Map<String, dynamic> json) =>
+      _$ClassifyMeValidationErrorFromJson(json);
+}
+
+@freezed
+class ClassifyMeFieldValidationError with _$ClassifyMeFieldValidationError {
+  factory ClassifyMeFieldValidationError(
+    String key,
+    List<String> errors,
+  ) = _ClassifyMeFieldValidationError;
+
+  factory ClassifyMeFieldValidationError.fromJson(Map<String, dynamic> json) =>
+      _$ClassifyMeFieldValidationErrorFromJson(json);
+}
+
+class Failure implements Exception {
+  Failure({
+    required this.message,
+    this.errors = const [],
+  });
+
+  final String message;
+  List<ClassifyMeFieldValidationError> errors;
+
+  List<String> errorMessages() {
+    final _errors = <String>[];
+
+    for (final _error in errors) {
+      _errors.add(
+        _error.errors[0],
+      );
+    }
+
+    return List.castFrom<dynamic, String>(_errors);
+  }
+}
