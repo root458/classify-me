cf4a43c7046c5d7e13ffe763e5352c93bf2f9404
Hope Kagame <hopekagame1@gmail.com>
2023-05-07 11:59:38 +0300

    Initialize Firebase

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,11 +1,17 @@
 import 'package:classifyme/bootstrap.dart';
 import 'package:classifyme/classifyme/classifyme.dart';
+import 'package:classifyme/firebase_options.dart';
 import 'package:classifyme/utils/_index.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   ClassifyMeConfig(
     values: ClassifyMeValues(
