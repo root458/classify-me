ca9c293f81e59d8331a6c855df1fff96490ec56c
Hope Kagame <hopekagame1@gmail.com>
2023-05-10 00:20:27 +0300

    Add enums

--- lib/utils/_index.dart
+++ lib/utils/_index.dart
@@ -4,10 +4,13 @@ import 'dart:io';
 
 import 'package:classifyme/features/step_three/cubit/get_course_recommendation_cubit.dart';
 import 'package:classifyme/models/_index.dart';
+import 'package:classifyme/notifiers/_index.dart';
 import 'package:classifyme/services/course_service.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
 
 part 'constants.dart';
+part 'enums.dart';
 part 'network.dart';
 part 'singletons.dart';
---
+++ lib/utils/enums.dart
@@ -0,0 +1,10 @@
+part of classifyme_utils;
+
+enum Interest {
+  none,
+  pharmacy,
+  surgery,
+  laboratories,
+  physiotherapy,
+  nursing,
+}
