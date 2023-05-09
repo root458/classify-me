288bd0a398e47a6175b4e1d1ca858d3ea5b5f14b
Hope Kagame <hopekagame1@gmail.com>
2023-05-10 02:24:00 +0300

    Enable adding subject grade

--- lib/features/step_two/widgets/subject_item.dart
+++ lib/features/step_two/widgets/subject_item.dart
@@ -1,42 +1,79 @@
+import 'package:classifyme/notifiers/_index.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class SubjectItem extends StatelessWidget {
   const SubjectItem({
     super.key,
     required this.subject,
-    required this.grade,
     this.end = false,
   });
 
   final String subject;
-  final String grade;
   final bool end;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment:
-          end ? CrossAxisAlignment.end : CrossAxisAlignment.start,
-      children: [
-        Text(
-          subject,
-          style: const TextStyle(
-            fontFamily: 'InterSemiBold',
-            color: Color(0xFFFFFFFF),
-            fontSize: 20,
+    // final height = MediaQuery.of(context).size.height;
+    // final width = MediaQuery.of(context).size.width;
+    final subjectsNotifier = Provider.of<SubjectsNotifier>(context);
+    final grades = <String>[
+      'A',
+      'A-',
+      'B+',
+      'B',
+      'B-',
+      'C+',
+      'C',
+      'C-',
+      'D+',
+      'D',
+      'D-',
+      'E'
+    ];
+
+    return PopupMenuButton<String>(
+      color: Color(0xFFF1EAE0),
+      elevation: 2,
+      position: PopupMenuPosition.under,
+      itemBuilder: (context) => [
+        for (final grade in grades)
+          PopupMenuItem(
+            height: 30,
+            value: grade,
+            child: Text(
+              grade,
+              style: TextStyle(color: Color(0xFF512ABA)),
+            ),
           ),
-        ),
-        const SizedBox(height: 10),
-        Text(
-          grade,
-          style: const TextStyle(
-            fontFamily: 'InterSemiBold',
-            color: Color(0xFFFFFFFF),
-            fontSize: 20,
-          ),
-        ),
       ],
+      onSelected: (grade) {
+        subjectsNotifier.setSubjectGrade(subject, grade);
+      },
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment:
+            end ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        children: [
+          Text(
+            subject,
+            style: const TextStyle(
+              fontFamily: 'InterSemiBold',
+              color: Color(0xFFFFFFFF),
+              fontSize: 20,
+            ),
+          ),
+          const SizedBox(height: 10),
+          Text(
+            subjectsNotifier.subjects[subject]!,
+            style: const TextStyle(
+              fontFamily: 'InterSemiBold',
+              color: Color(0xFFFFFFFF),
+              fontSize: 20,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
