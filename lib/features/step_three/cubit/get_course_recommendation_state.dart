dbebbea96cda093dc81213aaaa39f38709bdf1de
Hope Kagame <hopekagame1@gmail.com>
2023-05-10 12:07:34 +0300

    Update recommendation states

--- lib/features/step_three/cubit/get_course_recommendation_state.dart
+++ lib/features/step_three/cubit/get_course_recommendation_state.dart
@@ -4,6 +4,6 @@ part of 'get_course_recommendation_cubit.dart';
 class GetCourseRecommendationState with _$GetCourseRecommendationState {
   const factory GetCourseRecommendationState.initial() = _Initial;
   const factory GetCourseRecommendationState.loading() = _Loading;
-  const factory GetCourseRecommendationState.loaded() = _Loaded;
+  const factory GetCourseRecommendationState.loaded(List<String> courses) = _Loaded;
   const factory GetCourseRecommendationState.error(String error) = _Error;
 }
