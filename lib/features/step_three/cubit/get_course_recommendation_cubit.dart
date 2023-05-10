bb930e114c639f93f16362f22ca43938cc53329e
Hope Kagame <hopekagame1@gmail.com>
2023-05-10 15:35:07 +0300

    Get recommended courses

--- lib/features/step_three/cubit/get_course_recommendation_cubit.dart
+++ lib/features/step_three/cubit/get_course_recommendation_cubit.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
 
 import 'package:bloc/bloc.dart';
+import 'package:classifyme/models/_index.dart';
 import 'package:classifyme/services/course_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'get_course_recommendation_state.dart';
@@ -20,15 +22,34 @@ class GetCourseRecommendationCubit extends Cubit<GetCourseRecommendationState> {
     Map<String, int> subjectPerformance,
     String interest,
   ) async {
+    final FirebaseFirestore firestore = FirebaseFirestore.instance;
     emit(const GetCourseRecommendationState.loading());
     try {
-      // ignore: unused_local_variable
-      final _result = await _courseService.getCourseRecommendations();
+      final QuerySnapshot querySnapshot =
+          await firestore.collection('courses').get();
+      final QueryDocumentSnapshot doc = querySnapshot.docs.first;
+      List<dynamic> courses = doc.get(interest) as List<dynamic>;
 
-      emit(GetCourseRecommendationState.loaded());
+      List<String> recommendedCourses = courses.cast<String>();
+
+      // ignore: unused_local_variable
+      final _result = await _courseService.getCourseRecommendations(
+        UserData(
+          subjectPerformance['English']!,
+          subjectPerformance['Kiswahili']!,
+          subjectPerformance['Mathematics']!,
+          subjectPerformance['Physics']!,
+          subjectPerformance['Biology']!,
+          subjectPerformance['Chemistry']!,
+          interest,
+        ),
+      );
+
+      emit(GetCourseRecommendationState.loaded(recommendedCourses));
     } on SocketException {
       emit(GetCourseRecommendationState.error('Check network connection!'));
     } catch (e) {
+      print(e.toString());
       emit(GetCourseRecommendationState.error('Retrieving courses failed'));
     }
   }
